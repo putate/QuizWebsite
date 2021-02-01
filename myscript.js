32a4fb432b4bdf8ae26bcabb80a5b7227d63e901
putate <77691124+putate@users.noreply.github.com>
2021-02-01 10:04:25 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
   $("#Q1B").click(function(){
 		$("#A1D").fadeToggle();
 	});
+ $("#Q2B").click(function(){
+		$("#A2D").fadeToggle();
+	});
+ $("#Q3B").click(function(){
+		$("#A3D").fadeToggle();
+	});
+ $("#Q4B").click(function(){
+		$("#A4D").fadeToggle();
+	});
+ $("#Q5B").click(function(){
+		$("#A5D").fadeToggle();
+	});
+
 
 });
